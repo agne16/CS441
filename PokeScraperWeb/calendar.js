4dd5a7f0c0f67f888cbd72a6816c7ab703bacc4c
T.J <agne16@up.edu>
2015-03-14 14:19:20 -0700

    added pokemon font use larger of phy/spe attack fixed bug in calendar (seconds == 0) scrapped idea for loading screen

--- PokeScraperWeb/calendar.js
+++ PokeScraperWeb/calendar.js
@@ -14,14 +14,14 @@ printTime(getTime());
 ////////////////////////////////////////////////////////////////////
 
 //Retrieves current system date and time
-//Returns an array of the values 
+//Returns an array of the values
 function getTime()
 {
-	var today = new Date();	
+	var today = new Date();
 	var seconds	= today.getSeconds();
 	var minutes	= today.getMinutes();
 	var hours = today.getHours();
-	
+
 	//formats hours into 12-hr format
 	if (hours === 0)
 	{
@@ -33,11 +33,11 @@ function getTime()
 		//converts 24-hr format to 12 hr-format
 		hours -= 12;
 	}
-	
+
 	var date = today.getDate();
 	var month = today.getMonth() + 1;	//(months start at 0)
 	var year = today.getFullYear();
-	
+
 	return [seconds, minutes, hours, date, month, year];
 }
 
@@ -47,32 +47,31 @@ function printTime(calendar)
 	//separate four digit year to 2+2 digit year
 	var yearFirst = calendar[5].toString().substring(0,2);
 	var yearSecond = calendar[5].toString().substring(2);
-	
+
 	//generate a string of xml code
 	var out = "";
 	out += 	"<img src='" + getImage(getEntry(calendar[2])) + "'> :"
 		+ 	"<img src='" + getImage(getEntry(calendar[1])) + "'> :"
-		+ 	"<img src='" + getImage(getEntry(calendar[0])) + "'><br>"		
+		+ 	"<img src='" + getImage(getEntry(calendar[0])) + "'><br>"
 		+ 	"<img src='" + getImage(getEntry(calendar[4])) + "'>/"
 		+ 	"<img src='" + getImage(getEntry(calendar[3])) + "'>/"
 		+ 	"<img src='" + getImage(getEntry(yearFirst)) + "'>"
-		+ 	"<img src='" + getImage(getEntry(yearSecond)) + "'>" 
+		+ 	"<img src='" + getImage(getEntry(yearSecond)) + "'>"
 		+ 	"<br>";
-		
+
 	out += 	"The time is: " + calendar[2] + ":"
 		+ pad(calendar[1]) + ":"
-		+ pad(calendar[0]) + "<br>The date is: "		
+		+ pad(calendar[0]) + "<br>The date is: "
 		+ calendar[4] + "/"
 		+ calendar[3] + "/"
 		+ yearFirst + yearSecond + "<br>";
 
 	//output generated string to div on html
 	document.getElementById("id01").innerHTML += out;
-	document.getElementById("id01").style.fontSize = "xx-large";
 }
 
 //Pads a number with leading zero if less than 10
-function pad(n) 
+function pad(n)
 {
     return (n < 10) ? ("0" + n) : n;
 }
--- PokeScraperWeb/functions.js
+++ PokeScraperWeb/functions.js
@@ -4,9 +4,9 @@ various items from the Pokemon API
 
 Note that by binding getEntry to a variable, you can access many attributes
 
-Ex: 	var dexJSON = getEntry(50) //Diglett's Entry
-		var attack = dexJSON.attack;
-		var weight = dexJSON.weight;
+Ex: var dexJSON = getEntry(50) //Diglett's Entry
+    var attack = dexJSON.attack;
+    var weight = dexJSON.weight;
 
 Be mindful of what units measurements come in.
 For example, the API gives weight in hectograms or some other weird junk.
@@ -15,78 +15,98 @@ So  weight/10 gives you kilograms
 and height/10 gives you meters
 **/
 
+var downloadedPokemon = [""];
+
 //Returns the JSON Formatted Pokedex Data given a Dex Number
 function getEntry(dexNumber)
 {
-	//create new http request
-	var xhr = new XMLHttpRequest();
-	var url = null;
-	var dexJSON = null;
+	if (dexNumber == 0)
+	{
+		return undefined;
+	}
 	
-	//create url to query server and get response
-	url = "http://pokeapi.co/api/v1/pokemon/" + dexNumber + "/";
-	xhr.open("GET", url, false);
-	xhr.send();
-	
-	//return the parsed response (JSON format)
-	return JSON.parse(xhr.response.valueOf());
+    if(downloadedPokemon[dexNumber] != undefined)
+    {
+        return downloadedPokemon[dexNumber];
+    }
+    
+    //create new http request
+    var xhr = new XMLHttpRequest();
+    var url = null;
+    var dexJSON = null;
+    
+    //create url to query server and get response
+    url = "http://pokeapi.co/api/v1/pokemon/" + dexNumber + "/";
+    xhr.open("GET", url, false);
+    xhr.send();
+    
+    var entry = JSON.parse(xhr.response.valueOf());
+    
+    //return the parsed response (JSON format)
+    downloadedPokemon[dexNumber] = entry;
+    return entry;
 }
 
 //Returns url to an image given a JSON formatted Pokedex Entry
 function getImage(dexJSON)
 {
-	//request the pokedex data for a particular pokedex entry number
-	var xhr = new XMLHttpRequest();
+	if (dexJSON == undefined)
+	{
+		return "Images/Pokeball.png"
+	}
 	
-	//extract the url for the array of urls for a pokemon's sprites
-	//select first entry in array and request for the sprite data
-	var imguri = "http://pokeapi.co" + dexJSON.sprites[0].resource_uri;
-	xhr.open("GET", imguri, false);	
-	xhr.send();
-	
-	//return the url of the image
-	return "http://pokeapi.co" + JSON.parse(xhr.response).image;
+    //request the pokedex data for a particular pokedex entry number
+    var xhr = new XMLHttpRequest();
+    
+    //extract the url for the array of urls for a pokemon's sprites
+    //select first entry in array and request for the sprite data
+    var imguri = "http://pokeapi.co" + dexJSON.sprites[0].resource_uri;
+    xhr.open("GET", imguri, false);	
+    xhr.send();
+    
+    //return the url of the image
+    return "http://pokeapi.co" + JSON.parse(xhr.response).image;
 }
 
 //returns the description of a Pokemon given a JSON and a generation (int)
 function getDesc(dexJSON, gen)
 {
-	var xhr = new XMLHttpRequest();
-	
-	//default description will be a generation 1 description
-	var dexurl = "http://pokeapi.co" + dexJSON.descriptions[0].resource_uri
-	
-	// extract the description of a particular generation
-	//
-	// the JSON has an array of description objects that contains:
-	// 1) name of the object (pokemonName_gen_1, pokemonName_gen_3...) and
-	// 2) url to the actual text description
-	dexJSON.descriptions.forEach(function(entry)
-	{
-		//if the substring is found in the description name
-		if (entry.name.indexOf("gen_" + gen) != -1)
-		{			
-			dexurl = "http://pokeapi.co" + entry.resource_uri
-		}	
-	});
-	
-	//send request
-	xhr.open("GET", dexurl, false);
-	xhr.send();
-	
-	//return
-	return JSON.parse(xhr.response).description
+    var xhr = new XMLHttpRequest();
+    
+    //default description will be a generation 1 description
+    var dexurl = "http://pokeapi.co" + dexJSON.descriptions[0].resource_uri
+    
+    // extract the description of a particular generation
+    //
+    // the JSON has an array of description objects that contains:
+    // 1) name of the object (pokemonName_gen_1, pokemonName_gen_3...) and
+    // 2) url to the actual text description
+    dexJSON.descriptions.forEach(function(entry)
+                                 {
+        //if the substring is found in the description name
+        if (entry.name.indexOf("gen_" + gen) != -1)
+        {			
+            dexurl = "http://pokeapi.co" + entry.resource_uri
+        }	
+    });
+    
+    //send request
+    xhr.open("GET", dexurl, false);
+    xhr.send();
+    
+    //return
+    return JSON.parse(xhr.response).description
 }
 
-function calcRatio(hp,at,sat,spd)
+function calcRatio(hp, at, sat, spd)
 {
-	return Math.round((hp+at+sat)/spd)/10;
+    return Math.round((hp+at+sat)/spd)/10;
 }
 
 //Return a calculated BMI given height and weight (meters, kilograms)
 function calcBMI(height, weight)
 {
-	return Math.round((weight)/(height * height) * 10) / 10;
+    return Math.round((weight)/(height * height) * 10) / 10;
 }
 
 //Given a string, return the string	with first letter capitalized
@@ -97,31 +117,40 @@ function capitalizeFirstLetter(string)
 
 function getType1(dexJSON)
 {
-	//the array of types. may come in handy later, when we want to filter by type.
-	var typeArray = dexJSON.types;
-	
-	//Many Poker Manz are dual-typed. First, let's handle the first type.
-	var type1 = JSON.stringify(typeArray[0]); //type1 = what will eventually hold the type.
-	type1 = type1.substring(9, type1.length-1);
-	var lastChar = type1.indexOf('"');
-	type1 = type1.substring(0,lastChar);
-		
-	return capitalizeFirstLetter(type1);
-		
+    //the array of types. may come in handy later, when we want to filter by type.
+    var typeArray = dexJSON.types;
+    
+    //Many Poker Manz are dual-typed. First, let's handle the first type.
+    var type1 = JSON.stringify(typeArray[0]); //type1 = what will eventually hold the type.
+    type1 = type1.substring(9, type1.length-1);
+    var lastChar = type1.indexOf('"');
+    type1 = type1.substring(0,lastChar);
+    
+    return capitalizeFirstLetter(type1);
+    
 }
 
 function getType2(dexJSON)
 {
-	var typeArray = dexJSON.types;
-	
-	var type2 = JSON.stringify(typeArray[1]);
-	
-	if (type2!=undefined) 
-	{
-		type2 = type2.substring(9, type2.length-1);
-		lastChar=type2.indexOf('"');
-		type2=type2.substring(0,lastChar);		
-		return capitalizeFirstLetter(type2);
-	}
-	return "";
+    var typeArray = dexJSON.types;
+    
+    var type2 = JSON.stringify(typeArray[1]);
+    
+    if (type2!=undefined) 
+    {
+        type2 = type2.substring(9, type2.length-1);
+        lastChar=type2.indexOf('"');
+        type2=type2.substring(0,lastChar);		
+        return capitalizeFirstLetter(type2);
+    }
+    return "";
+}
+
+function isInArray(number)
+{
+    if(downloadPokemon[number] === undefined)
+    {
+        return true;
+    }
+    return false;
 }
