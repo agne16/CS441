01ee83598fa6bd7a445681f6fa350c1de8ba9d11
T.J <agne16@up.edu>
2015-04-18 16:14:27 -0700

    organized folders
    moved javascripts to Scripts
    moved older pages to Legacy
    separated javascript from bmi charts to new file

---
+++ PokeScraperWeb/Scripts/bmi-attack-chart.js
@@ -0,0 +1,322 @@
+var validNums = false;
+var start = 1;
+var end = 7;
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(drawChart);
+
+/**
+ *function previous(id)
+ *redraws the chart with the six previous Pokemon, in Dex order.
+ *@param id - the ID of the html element that called this function.
+ */
+function previous(id)
+{
+    if (start-6>0) {
+        start=start-6
+        end = start+6;
+        drawChart();
+    }
+}
+
+/**
+ *function next(id)
+ *redraws the chart with the six next Pokemon in National Dex order.
+ *@param id - the ID of the html element that called this function
+ */
+function next(id)
+{
+    if (end+6<649) {
+        end=end+6;
+        start=end-6;
+        
+        drawChart();
+    }
+}
+
+/**
+ *function searchByName
+ *searches the ENTIRE Pokedex for the name of the Pokemon entered in the input field
+ *with ID "searchThisName". Currently too slow.
+ *@param id - the id of the html element that called it.
+ */
+function searchByName(id)
+{
+    var found = false;
+    var name = document.getElementById("searchThisName").value;
+    var i = 0;
+    for(i=1;i<650;i++)
+    {
+        var dex = getEntry(i);
+        if (dex.name==name)
+        {
+            start = i;
+            end=i;
+            found = true;
+            drawChart();
+            alert("found");
+            break;
+        }
+    }
+    if (!found)
+    {
+        alert("Pokerman not found.");
+    }
+}
+
+/**
+ *function displayCustomRange
+ *Takes the text entered into the aptly named fields
+ *and redraws the chart with those indexes.
+ *@param id - the id of the html element that called this function.
+ */
+function displayCustomRange(id)
+{
+    var newStart = document.getElementById("startIdx").value;
+    var newEnd = document.getElementById("endIdx").value;
+    if (newStart>0 && newEnd<650)
+    {
+        start = parseInt(newStart);
+        end = parseInt(newEnd);
+        drawChart();
+    }
+    else
+    {
+        alert("Invalid inputs. Chart not redrawn. You dick.");
+    }
+}
+function drawChart()
+{ 
+    var data = new google.visualization.DataTable();
+
+    data.addColumn('number', 'x');
+
+    var types = 
+	[
+        "Normal",
+        "Fire",
+        "Water",
+        "Electric",
+        "Grass",
+        "Ice",
+        "Fighting",
+        "Poison",
+        "Ground",
+        "Flying",
+        "Psychic",
+        "Bug",
+        "Rock",
+        "Ghost",
+        "Dragon",
+        "Dark",
+        "Steel",
+        "Fairy",
+    ];
+
+    for (var i = 0; i < types.length; i++)
+    {
+        data.addColumn('number', types[i]);
+        data.addColumn({type: 'string', role: 'tooltip', 'p': {'html': true}});
+    }
+    
+    var vals = [];
+    for(var i = start;i<end;i++)
+    {
+        var dexJSON = getEntry(i);
+        var height = dexJSON.height / 10;
+        var weight = dexJSON.weight / 10;
+        //var ratio = calcRatio(dexJSON.hp,dexJSON.attack,dexJSON.sp_atk,dexJSON.speed);
+        var name = dexJSON.name;
+        var att = dexJSON.attack;
+        var bmi = calcBMI(height, weight);
+        var typeArray = dexJSON.types;
+        
+        //Protip: Theres a fuction to get types
+        //Many Poker Manz are dual-typed. First, let's handle the first type.
+        var type1 = getType1(dexJSON);
+        var colorType = type1;
+        
+        //now on to type2.
+        var type2 = getType2(dexJSON);	
+        if (type2!="")
+        {
+            colorType=type2;
+            type2=type2.substring(0,lastChar)+"/";
+        }
+
+        var finalType = type2+type1;
+        var image = '<img src="'+ getImage(dexJSON)+'"/>';
+        var htmlContent2 = '<table style="width:100%">'+
+            '<tr>'+
+            '<td>'+image+'</td>'+
+            '<td><table style="width:100%">'+
+            '<tr>'+
+            '<td>'+ name +'</td>' +
+            '</tr>'+
+            '<tr>'+
+            '<td>Attack: ' + att+'</td>'+
+            '</tr>'+
+            '<tr>'+
+            '<td>BMI: '+bmi+'</td>'+
+            '<tr>'+
+            '<td>Type(s) '+finalType+
+            '</tr>'+
+            '</table>' +
+            '</td>'+
+            '</tr>' +
+            '</table>'
+        /**
+        *This mess is how you add data points to the chart. Each data point has three paramters: 
+        *bmi, att, and tooltip (aka htmlContent2). bmi always goes in the first index. htmlContent2 always goes
+        *right after att. Where att goes depends on the Pokemon's type.
+        */
+
+        //initialize an array of null
+        var column = [];//not sure if the right var name. seems right
+        var attPos = -1;
+        for(var j=0; j < 37; j++)
+        {
+            column[j] = null;
+        }
+
+        //figure out index that attack will be stored in array
+        if (colorType=="Normal")
+        {
+            attPos = 1;
+        }
+        else if (colorType=="Fire")
+        {
+            attPos = 3;
+        }
+        else if (colorType=="Water")
+        {
+            attPos = 5;
+        }
+        else if (colorType=="Electric")
+        {
+            attPos = 7;
+        }
+        else if (colorType=="Grass")
+        {
+            attPos = 9;
+        }
+        else if (colorType=="Ice")
+        {
+            attPos = 11;
+        }
+        else if (colorType=="Fighting")
+        {
+            attPos = 13;
+        }
+        else if (colorType=="Poison")
+        {
+            attPos = 15;
+        }
+        else if (colorType=="Ground")
+        {
+            attPos = 17;
+        }
+        else if (colorType=="Flying")
+        {
+            attPos = 19;
+        }
+        else if (colorType=="Psychic")
+        {
+            attPos = 21;
+        }
+        else if (colorType=="Bug")
+        {
+            attPos = 23;
+        }
+        else if (colorType=="Rock")
+        {
+            attPos = 25;
+        }
+        else if (colorType=="Ghost")
+        {
+            attPos = 27;
+        }
+        else if (colorType=="Dragon")
+        {
+            attPos = 29;
+        }
+        else if (colorType=="Dark")
+        {
+            attPos = 31;
+        }
+        else if (colorType=="Steel")
+        {
+            attPos = 33;
+        }
+        else if (colorType=="Fairy")
+        {
+            attPos = 35;
+        }
+
+        //if a type was found
+        if(attPos != -1)
+        {
+            //fill in column and add it to table
+            column[0] = bmi;
+            column[attPos] = att;
+            column[attPos + 1] = htmlContent2;
+            vals.push(column);
+        }
+
+    }//end of gathering all pokedex entries
+    
+    data.addRows(vals);
+    var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
+    var options =
+        {
+            title: "Attack vs BMI",
+            vAxis:
+            {
+                title: "Attack",
+                viewWindow:
+                {
+                    min: 0
+                }
+
+            },
+            hAxis:
+            {
+                title: 'BMI',
+                viewWindow:
+                {
+                    min: 0
+                }
+            },
+
+            series:
+            {                
+                0  : { color: '#A8A77A'}, //Normal    
+                1  : { color: '#EE8130'}, //Fire      
+                2  : { color: '#6390F0'}, //Water     
+                3  : { color: '#F7D02C'}, //Electric   
+                4  : { color: '#7AC74C'}, //Grass     
+                5  : { color: '#96D9D6'}, //Ice       
+                6  : { color: '#C22E28'}, //Fighting   
+                7  : { color: '#A33EA1'}, //Poison    
+                8  : { color: '#E2BF65'}, //Ground    
+                9  : { color: '#A98FF3'}, //Flying    
+                10 : { color: '#F95587'}, //Psychic   
+                11 : { color: '#A6B91A'}, //Bug       
+                12 : { color: '#B6A136'}, //Rock      
+                13 : { color: '#735797'}, //Ghost     
+                14 : { color: '#6F35FC'}, //Dragon    
+                15 : { color: '#705746'}, //Dark      
+                16 : { color: '#B7B7CE'}, //Steel     
+                17 : { color: '#D685AD'}, //Fairy     
+            },
+
+            tooltip:
+            {
+                isHtml: true
+            },
+
+            pointSize:20
+        }//options
+
+    chart.draw(data, options);
+    //{width: 400, height: 240, titleX: 'Attack', titleY: 'BMI', legend: 'none', pointSize: 5}
+}//drawChart
--- PokeScraperWeb/Scripts/functions.js
+++ PokeScraperWeb/Scripts/functions.js
@@ -23,23 +23,23 @@ function getEntry(dexNumber)
 	{
 		return undefined;//NOPE
 	}
-	
+
 	//check if the data is in session storage first
     if(sessionStorage.getItem(dexNumber))
     {
         return JSON.parse(sessionStorage.getItem(dexNumber));
     }
-    
+
     //create new http request
     var xhr = new XMLHttpRequest();
     var url = null;
     var entry = null;
-    
+
     //create url to query server and get response
     url = "http://pokeapi.co/api/v1/pokemon/" + dexNumber + "/";
     xhr.open("GET", url, false);
     xhr.send();
-    
+
 	//store the pokemon data and return it
     entry = JSON.parse(xhr.response.valueOf());
     sessionStorage.setItem(dexNumber, JSON.stringify(entry));
@@ -52,62 +52,63 @@ function getImage(dexJSON)
 	//if you want to get the image to a non-existant entry
 	if (dexJSON == undefined)
 	{
-		return "Images/Pokeball.png"
+		return "../Images/Pokeball.png"
 	}
-	
+
 	//see if the url image is in sessionstorage
 	var key = dexJSON.national_id + "_image";
 	if(sessionStorage.getItem(key))
     {
         return sessionStorage.getItem(key);
     }
-	
+
     //request the pokedex data for a particular pokedex entry number
     var xhr = new XMLHttpRequest();
-    
+
     //extract the url for the array of urls for a pokemon's sprites
     //select first entry in array and request for the sprite data
     var imguri = "http://pokeapi.co" + dexJSON.sprites[0].resource_uri;
-    xhr.open("GET", imguri, false);	
+    xhr.open("GET", imguri, false);
     xhr.send();
-    
+
     //store the url of the image and return it
     var image = "http://pokeapi.co" + JSON.parse(xhr.response).image;
 	sessionStorage.setItem(key, image);
     return image;
 }
 
-//returns the description of a Pokemon given a JSON and a generation (int)
+//Returns the description of a Pokemon given a JSON and a generation (int)
 function getDesc(dexJSON, gen)
 {
     var xhr = new XMLHttpRequest();
-    
+
     //default description will be a generation 1 description
     var dexurl = "http://pokeapi.co" + dexJSON.descriptions[0].resource_uri
-    
+
     // extract the description of a particular generation
     //
     // the JSON has an array of description objects that contains:
     // 1) name of the object (pokemonName_gen_1, pokemonName_gen_3...) and
     // 2) url to the actual text description
-    
+
 	dexJSON.descriptions.forEach(function(entry)
     {
         //if the substring is found in the description name
         if (entry.name.indexOf("gen_" + gen) != -1)
-        {			
+        {
             dexurl = "http://pokeapi.co" + entry.resource_uri
-        }	
+        }
     });
-    
+
     //send request
     xhr.open("GET", dexurl, false);
     xhr.send();
-    
+
     //return
     return JSON.parse(xhr.response).description
 }
 
+//Calculate combat value of a Pokemon
 function calcRatio(hp, at, sat, spd)
 {
     return Math.round((hp+at+sat)/spd)/10;
@@ -120,47 +121,42 @@ function calcBMI(height, weight)
 }
 
 //Given a string, return the string	with first letter capitalized
-function capitalizeFirstLetter(string) 
+function capitalizeFirstLetter(string)
 {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+//Extract the first type of the Pokemon given its raw data
+//This is the primary type if it's a single type
 function getType1(dexJSON)
 {
     //the array of types. may come in handy later, when we want to filter by type.
     var typeArray = dexJSON.types;
-    
+
     //Many Poker Manz are dual-typed. First, let's handle the first type.
     var type1 = JSON.stringify(typeArray[0]); //type1 = what will eventually hold the type.
     type1 = type1.substring(9, type1.length-1);
     var lastChar = type1.indexOf('"');
     type1 = type1.substring(0,lastChar);
-    
+
     return capitalizeFirstLetter(type1);
-    
+
 }
 
+//Extract the second type of the Pokemon given its raw data
+//This is the primary type if it exists
 function getType2(dexJSON)
 {
     var typeArray = dexJSON.types;
-    
+
     var type2 = JSON.stringify(typeArray[1]);
-    
-    if (type2!=undefined) 
+
+    if (type2!=undefined)
     {
         type2 = type2.substring(9, type2.length-1);
         lastChar=type2.indexOf('"');
-        type2=type2.substring(0,lastChar);		
+        type2=type2.substring(0,lastChar);
         return capitalizeFirstLetter(type2);
     }
     return "";
-}
-
-function isInArray(number)
-{
-    if(downloadPokemon[number] === undefined)
-    {
-        return true;
-    }
-    return false;
 }
