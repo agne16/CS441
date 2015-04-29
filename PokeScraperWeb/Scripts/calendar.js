246d8de477a4d127667fc33456a008de26567b8e
T.J <agne16@up.edu>
2015-04-29 01:05:47 -0700

    Nodejs + Heroku deployment files added
    main page renamed to index.html (to work with Nodejs)
    renamed bmi script to just pokescraper

---
+++ PokeScraperWeb/Scripts/calendar.js
@@ -0,0 +1,77 @@
+/**
+Simple script that converts date and time to pokemon sprites
+Requires the functions from functions.js
+
+Dependencies:
+	functions.js
+*/
+
+//get current time then print it
+printTime(getTime());
+
+////////////////////////////////////////////////////////////////////
+//////////////////////////FUNCTIONS/////////////////////////////////
+////////////////////////////////////////////////////////////////////
+
+//Retrieves current system date and time
+//Returns an array of the values
+function getTime()
+{
+	var today = new Date();
+	var seconds	= today.getSeconds();
+	var minutes	= today.getMinutes();
+	var hours = today.getHours();
+
+	//formats hours into 12-hr format
+	if (hours === 0)
+	{
+		//midnight (00:00) is 12:00
+		hours = 12;
+	}
+	else if (hours > 12)
+	{
+		//converts 24-hr format to 12 hr-format
+		hours -= 12;
+	}
+
+	var date = today.getDate();
+	var month = today.getMonth() + 1;	//(months start at 0)
+	var year = today.getFullYear();
+
+	return [seconds, minutes, hours, date, month, year];
+}
+
+//Prints the date/time as images provided an array of data
+function printTime(calendar)
+{
+	//separate four digit year to 2+2 digit year
+	var yearFirst = calendar[5].toString().substring(0,2);
+	var yearSecond = calendar[5].toString().substring(2);
+
+	//generate a string of xml code
+	var out = "";
+	out += 	"<img src='" + getImage(getEntry(calendar[2])) + "'> :"
+		+ 	"<img src='" + getImage(getEntry(calendar[1])) + "'> :"
+		+ 	"<img src='" + getImage(getEntry(calendar[0])) + "'><br>"
+		+ 	"<img src='" + getImage(getEntry(calendar[4])) + "'>/"
+		+ 	"<img src='" + getImage(getEntry(calendar[3])) + "'>/"
+		+ 	"<img src='" + getImage(getEntry(yearFirst)) + "'>"
+		+ 	"<img src='" + getImage(getEntry(yearSecond)) + "'>"
+		+ 	"<br>";
+
+	out += 	"The time is: " + calendar[2] + ":"
+		+ pad(calendar[1]) + ":"
+		+ pad(calendar[0]) + "<br>The date is: "
+		+ calendar[4] + "/"
+		+ calendar[3] + "/"
+		+ yearFirst + yearSecond + "<br>";
+
+	//output generated string to div on html
+	document.getElementById("id01").innerHTML += out;
+}
+
+//Pads a number with leading zero if less than 10
+function pad(n)
+{
+    return (n < 10) ? ("0" + n) : n;
+}
---
+++ PokeScraperWeb/Scripts/stats.js
@@ -0,0 +1,103 @@
+/**
+This script iterates over a range of pokemon to get the following:
+Image
+ID
+NAME
+STATS
+BMI is calculated and the data is used to create an HTML table
+Dependencies:
+	functions.js
+*/
+
+//buildTable is the entry point of script, just supply a start and end ID
+
+////////////////////////////////////////////////////////////////////
+//////////////////////////FUNCTIONS/////////////////////////////////
+////////////////////////////////////////////////////////////////////
+function buildTable(start, end)
+{
+	//initialize first row of table
+	var out = '<table>';
+	out += "<tr>"
+		+ "<td>SPRITE</td>"
+		+ "<td>ID</td>"
+		+ "<td>NAME</td>"
+		+ "<td>TYPE</td>"
+		+ "<td>HP</td>"
+		+ "<td>ATTACK</td>"
+		+ "<td>DEFENSE</td>"
+		+ "<td>SPEC-ATK</td>"
+		+ "<td>SPEC-DEF</td>"
+		+ "<td>SPEED</td>"
+		+ "<td>COMBAT VALUE</td>"
+		+ "<td>BMI</td>"
+		+ "</tr>";
+		
+	//iterate over a range of pokedex entries
+	for(var i = start; i <= end; ++i)
+	{
+		var dexJSON = getEntry(i);
+		var height = dexJSON.height / 10;
+		var weight = dexJSON.weight / 10;
+		var ratio = calcRatio(dexJSON.hp,dexJSON.attack,dexJSON.sp_atk,dexJSON.speed);
+		var name = dexJSON.name;
+		var url = "http://bulbapedia.bulbagarden.net/wiki/"+name;
+		
+		//the array of types. may come in handy later, when we want to filter by type.
+		var typeArray = dexJSON.types;
+		//Many Poker Manz are dual-typed. First, let's handle the first type.
+		var type1 = JSON.stringify(typeArray[0]); //type1 = what will eventually hold the type.
+		type1 = type1.substring(9, type1.length-1);
+		var lastChar = type1.indexOf('"');
+		type1 = type1.substring(0,lastChar);
+		
+		//now on to type2.
+		var type2 = JSON.stringify(typeArray[1]);
+		
+		if (type2!=undefined) {
+			type2 = type2.substring(9, type2.length-1);
+			lastChar=type2.indexOf('"');			
+			type2 = capitalizeFirstLetter(type2);
+			type2=type2.substring(0,lastChar)+"/";
+		}
+		
+		//if it's undefined, set it equal to none.
+		if (type2==undefined) {
+			type2 = "";
+		}
+		
+		type1 = capitalizeFirstLetter(type1);
+		
+		
+		var finalType = type2+type1;
+		
+		out += "<tr>"
+			+ "<td width='150'>"+ "<a href='" + url +  "'target='_blank'</a><img src='" + getImage(dexJSON) + "'/></td>"
+			+ "<td>" + dexJSON.national_id + "</td>"
+			+ "<td width='100'>" + name + "</td>"
+			+ "<td>" + finalType+ "</td>"
+			+ "<td>" + dexJSON.hp+ "</td>"
+			+ "<td>" + dexJSON.attack+ "</td>"
+			+ "<td>" + dexJSON.defense + "</td>"
+			+ "<td>" + dexJSON.sp_atk + "</td>"
+			+ "<td>" + dexJSON.sp_def + "</td>"
+			+ "<td>" + dexJSON.speed + "</td>"
+			+ "<td>" + ratio + "</td>"
+			+ "<td>" + calcBMI(height,weight) + "</td>"
+			+ "</tr>";
+	}
+	
+	out += "</table>";
+	document.getElementById("id01").innerHTML += out
+}
+
+function calcRatio(hp,at,sat,spd)
+{
+	return Math.round((hp+at+sat)/spd)/10;
+}
+
+//Return a calculated BMI given height and weight (meters, kilograms)
+function calcBMI(height, weight)
+{
+	return Math.round((weight)/(height * height) * 10) / 10;
+}
